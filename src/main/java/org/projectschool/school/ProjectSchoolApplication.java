77dd778c222b69e0350c473449642b33fc6c9d36
Ronald Marin <romasa000@gmail.com>
2018-09-11 11:57:30 -0600

    Inicializando proyecto.

---
+++ src/main/java/org/projectschool/school/ProjectSchoolApplication.java
@@ -0,0 +1,12 @@
+package org.projectschool.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSchoolApplication.class, args);
+	}
+}
